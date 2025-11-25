1b974685f185e5d4ff103f6692d1f5a6413dccd6
Shema Alphonse <ashema330@gmail.com>
2025-11-25 19:32:45 +0200

    displaying menu

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,42 @@
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner =new Scanner(System.in);
     public static void main(String[] args){
-        Scanner scanner =new Scanner(System.in);
+
         int choice =0;
+
 
         do{
             displayMenu();
-            System.out.println("Enter choice (1-5): ");
+            System.out.print("Enter choice (1-5): ");
             choice = scanner.nextInt();
 
             while(choice <1 || choice >5){
                 System.out.println("Enter valid choice: ");
-                System.out.println("Enter choice (1-5): ");
+                System.out.print("Enter choice (1-5): ");
                 choice = scanner.nextInt();
             }
             switch (choice){
-                case 1 -> System.out.println("Create New Account");
+                case 1 -> createAccount();
                 case 2 -> System.out.println("View All Accounts");
                 case 3 -> System.out.println("Process Transaction");
                 case 4 -> System.out.println("View Transaction History");
-                case 5 -> System.out.println("Exit");
-                default -> System.out.println("Enter valid choice");
+                case 5 -> System.out.println("\nThank you for using the Bank Account Management System. Goodbye!");
+                default -> System.out.println("Invalid choice. Please enter a number between 1 and 5.");
+
             }
 
+        }while(true);
 
-        }while (true);
     }
+
     // --- Helper Methods ---
-    private static void displayMenu(){
+    private static void displayMenu() {
         System.out.println("\n=============================================");
-        System.out.println("      BANK ACCOUNT MANAGEMENT SYSTEM MENU    ");
+        System.out.println("||                                          ||");
+        System.out.println("||    BANK ACCOUNT MANAGEMENT SYSTEM MENU   ||");
+        System.out.println("||                                          ||");
         System.out.println("=============================================");
         System.out.println("1. Create New Account ");
         System.out.println("2. View All Accounts ");
@@ -38,5 +44,25 @@ public class Main {
         System.out.println("4. View Transaction History ");
         System.out.println("5. Exit");
         System.out.println("=============================================");
+    }
+    private static void initializeDate(){}
+    private static void createAccount(){
+
+        System.out.println("\n--- CREATE NEW ACCOUNT ---");
+        System.out.print("Enter Customer Name: ");
+        String name = scanner.next();
+
+        System.out.print("Enter ege: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter Customer Contact: ");
+        String contact = scanner.nextLine();
+
+        System.out.print("Enter Customer Address: ");
+        String address = scanner.nextLine();
+
+        System.out.printf("You names are %s and you are %d years old\n",name,age);
+        System.out.printf("You phone number is %s and you are have address %s ",contact,address);
     }
 }
