3cb4d44f3e6b83d944a7b3916b4ad6b7e21a933e
Shema Alphonse <ashema330@gmail.com>
2025-11-25 19:35:14 +0200

    RegularCustomer class for US-2

--- src/RegularCustomer.java
+++ src/RegularCustomer.java
@@ -1,2 +1,23 @@
-public class RegularCustomer {
+public class RegularCustomer extends Customer{
+
+    // Constructor (simply calls the abstract class constructor)
+    public RegularCustomer(String contact, String name, int age, String address) {
+        super(contact, name, age, address);
+    }
+
+    // Override the abstract method to display customer details
+    @Override
+    public void displayCustomerDetails() {
+        System.out.println("Customer ID: " + getCustomerId());
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
+        System.out.println("Contact: " + getContact());
+        System.out.println("Type: " + getCustomerType() + " (Standard banking services)");
+    }
+
+    // Override the abstract method to specify the type
+    @Override
+    public String getCustomerType() {
+        return "Regular";
+    }
 }
