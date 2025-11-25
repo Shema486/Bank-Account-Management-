fab99196e42c504ae9ddcc2f01ad892fa2fd6be0
Shema Alphonse <ashema330@gmail.com>
2025-11-25 19:34:07 +0200

    Customer class for US-2

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,32 @@
-public class Customer {
+public abstract class Customer {
+    // Static field for generating unique customer IDs (e.g., CUS001)
+    public static int customerCounter=0;
+
+    // Private fields (Encapsulation)
+    private String customerId;
+    private int age;
+    private String name;
+    private String contact;
+    private String address;
+
+    //constructor
+    public Customer(String contact, String name, int age, String address) {
+        customerCounter++;
+        this.customerId =String.format("CUS%03d",customerCounter) ;
+        this.contact = contact;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    // Abstract methods for polymorphism
+     public abstract void displayCustomerDetails();
+     public abstract  String getCustomerType();
+
+    // Getters and Setters
+    public String getAddress() {return address;}
+    public String getContact() {return contact;}
+    public String getName() {return name;}
+    public int getAge() {return age;}
+    public String getCustomerId() {return customerId;}
 }
