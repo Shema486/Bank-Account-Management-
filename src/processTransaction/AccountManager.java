3cdf91e572e37a88e40b7bf5516f11b85e06eb0c
Shema Alphonse <ashema330@gmail.com>
2025-11-27 20:07:24 +0200

    Implement AccountManager for storing Account objects and linear search functionality.

--- src/processTransaction/AccountManager.java
+++ src/processTransaction/AccountManager.java
@@ -1,9 +1,63 @@
 package processTransaction;
+import accounts.Account;
+import customers.RegularCustomer;
 
 public class AccountManager {
     // Composition: Holds an array of Account objects
+    private Account[] accounts;
+    private int accountCount;
+    private final int MAX_ACCOUNTS = 50;
+
+    public AccountManager() {
+        this.accounts = new Account[MAX_ACCOUNTS];
+        this.accountCount = 0;
+    }
+
+    // Method for US-2 (Create Account)
+        public void addAccount(Account newAccount) {
+            if (accountCount < accounts.length) {
+                accounts[accountCount] = newAccount;
+                accountCount++;
+//                System.out.println("Account added successfully!");
+            } else {
+                System.out.println("Bank is full! Cannot add more accounts.");
+            }
+    }
+
+    // Method for US-3 (Process Transaction) - DSA: Linear Search
+     public Account findAccount(String accountNumber) {
+        for (int i =0; i<accountCount;i++){
+             if(accounts[i].getAccountNumber().equals(accountNumber)){
+                return accounts[i];
+               }
+            }
+             return null;
+        }
 
 
+    // Method to calculate total balance
+    public double getTotalBalance() {
+        double total = 0;
+        for (int i = 0; i < accountCount; i++) {
+            total += accounts[i].getBalance();  // Add each account's balance
+        }
+        return total;
+    }
+    // Method for US-1 (View Accounts)
+    public void viewAllAccounts() {
+        if (accountCount == 0) {
+            System.out.println("No accounts in the bank.");
+            return;
+        }
 
+        System.out.println("----- All Bank Accounts -----");
+        for (int i = 0; i < accountCount; i++) {
+            System.out.println("Account #" + (i + 1));
+            accounts[i].displayAccountDetails();
+            System.out.println("----------------------------");
+        }
+    }
 
+    // Getter
+    public int getAccountCount() {return accountCount;}
 }
