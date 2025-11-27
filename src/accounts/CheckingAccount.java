ce448c182bd34a2b09a081bd2e390cab77f8659f
Shema Alphonse <ashema330@gmail.com>
2025-11-27 20:00:12 +0200

    FEAT: Implement CheckingAccount with overdraft limit ($1000) withdrawal rule.

--- src/accounts/CheckingAccount.java
+++ src/accounts/CheckingAccount.java
@@ -1,4 +1,46 @@
 package accounts;
 
-public class CheckingAccount {
+import customers.Customer;
+
+public class CheckingAccount extends Account{
+
+    private double overdraftLimit;
+    private double monthlyFee;
+
+    public CheckingAccount(Customer customer, double initialBalance) {
+        super(customer, initialBalance);
+        this.overdraftLimit = 1000;
+        this.monthlyFee =10;
+    }
+
+    //Menu after creating checkingAccount
+    @Override
+    public void displayAccountDetails() {
+        System.out.println("ACC NO: "+getAccountNumber());
+        System.out.println("CUSTOMER NAME: "+getCustomer().getName());
+        System.out.println("TYPE: "+getStatus());
+        System.out.print("BALANCE: "+getBalance());
+        System.out.println("STATUS: "+getStatus());
+        System.out.printf("OVERDRAFT LIMIT: $%,.2f" , overdraftLimit);
+        System.out.printf("Monthly fee $%,.2f",monthlyFee);
+        System.out.println("\n"+appyMonthlyFee());
+    }
+    //return type of account
+    @Override
+    public String getAccountType() {return "Checking";}
+    @Override
+    public void withdraw(double amount) {
+        // Check if withdrawal stays within overdraft limit
+        if (this.balance - amount >= overdraftLimit) {
+            this.balance -= amount;
+            System.out.println("Withdrawal successful. New balance: $" + this.balance);
+        } else {
+            System.out.println("Withdrawal denied! Overdraft limit of $1000 exceeded.");
+        }
+    }
+
+    public String appyMonthlyFee(){
+        balance -= monthlyFee;
+        return "Monthly fee of $10 applied. New balance: $" + balance;
+    }
 }
